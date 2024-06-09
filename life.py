6d6d65f91077b7b96dfcef40b297db20033f1a3d
Byteinsite <byteinsite@yandex.ru>
2024-06-09 22:37:16 +0300

    Migrate to classes

--- life.py
+++ life.py
@@ -3,75 +3,71 @@ import time
 import os
 import platform
 
-# Определяем размеры поля
-width, height = 20, 20
-# Проверяем и записываем на какой ОС запущена программа
-cmd_clear = 'cls' if platform.system() == 'Windows' else 'clear'
+class GameOfLife:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.grid = self.initialize_grid()
+        self.clear_command = 'cls' if platform.system() == "Windows" else 'clear'
 
+    def initialize_grid(self):
+        return np.random.choice([0, 1], size=(self.width, self.height))
 
-# Инициализация случайного начального состояния
-def initialize_grid(w, h):
-    return np.random.choice([0, 1], size=(w, h))
+    def print_grid(self, intermediate_grid=None):
+        # Очистка консоли
+        os.system(self.clear_command)
 
-
-# Функция для отображения состояния поля
-def print_grid(grid, intermediate_grid=None):
-    for i in range(grid.shape[0]):
-        row_str = ''
-        for j in range(grid.shape[1]):
-            if intermediate_grid is not None:
-                if intermediate_grid[i, j] == 1:
-                    row_str += '□ '
-                elif intermediate_grid[i, j] == 2:
-                    row_str += '░ '
+        for i in range(self.grid.shape[0]):
+            row_str = ''
+            for j in range(self.grid.shape[1]):
+                if intermediate_grid is not None:
+                    if intermediate_grid[i, j] == 1:
+                        row_str += '□ '
+                    elif intermediate_grid[i, j] == 2:
+                        row_str += '░ '
+                    else:
+                        row_str += '█ ' if self.grid[i, j] else '  '
                 else:
-                    row_str += '█ ' if grid[i, j] else '  '
-            else:
-                row_str += '█ ' if grid[i, j] else '  '
-        print(row_str)
-    print("\n")
+                    row_str += '█ ' if self.grid[i, j] else '  '
+            print(row_str)
+        print("\n")
 
-# Функция для создания промежуточного кадра
-def create_intermediate_grid(grid):
-    intermediate_grid = np.zeros(grid.shape)
-    for i in range(grid.shape[0]):
-        for j in range(grid.shape[1]):
-            live_neighbors = np.sum(grid[i-1:i+2, j-1:j+2]) - grid[i, j]
-            if grid[i, j] == 1:
-                if live_neighbors < 2 or live_neighbors > 3:
-                    intermediate_grid[i, j] = 1  # Умрет
-            else:
-                if live_neighbors == 3:
-                    intermediate_grid[i, j] = 2  # Оживёт
-    return intermediate_grid
+    def create_intermediate_grid(self):
+        intermediate_grid = np.zeros(self.grid.shape)
+        for i in range(self.grid.shape[0]):
+            for j in range(self.grid.shape[1]):
+                live_neighbors = np.sum(self.grid[i-1:i+2, j-1:j+2]) - self.grid[i, j]
+                if self.grid[i, j] == 1:
+                    if live_neighbors < 2 or live_neighbors > 3:
+                        intermediate_grid[i, j] = 1  # Умрет
+                else:
+                    if live_neighbors == 3:
+                        intermediate_grid[i, j] = 2  # Оживёт
+        return intermediate_grid
 
-# Обновление состояния поля
-def update_grid(grid):
-    new_grid = grid.copy()
-    for i in range(grid.shape[0]):
-        for j in range(grid.shape[1]):
-            live_neighbors = np.sum(grid[i-1:i+2, j-1:j+2]) - grid[i, j]
-            if grid[i, j] == 1:
-                if live_neighbors < 2 or live_neighbors > 3:
-                    new_grid[i, j] = 0
-            else:
-                if live_neighbors == 3:
-                    new_grid[i, j] = 1
-    return new_grid
+    def update_grid(self):
+        new_grid = self.grid.copy()
+        for i in range(self.grid.shape[0]):
+            for j in range(self.grid.shape[1]):
+                live_neighbors = np.sum(self.grid[i-1:i+2, j-1:j+2]) - self.grid[i, j]
+                if self.grid[i, j] == 1:
+                    if live_neighbors < 2 or live_neighbors > 3:
+                        new_grid[i, j] = 0
+                else:
+                    if live_neighbors == 3:
+                        new_grid[i, j] = 1
+        self.grid = new_grid
 
-# Основной цикл игры
-def game_of_life(width, height, iterations):
-    grid = initialize_grid(width, height)
-    for _ in range(iterations):
-        os.system(cmd_clear)
-        print_grid(grid)
-        intermediate_grid = create_intermediate_grid(grid)
-        time.sleep(1)
-        os.system(cmd_clear)
-        print_grid(grid, intermediate_grid)
-        time.sleep(1)
-        grid = update_grid(grid)
+    def run(self, iterations):
+        for _ in range(iterations):
+            self.print_grid()
+            intermediate_grid = self.create_intermediate_grid()
+            time.sleep(0.5)
+            self.print_grid(intermediate_grid)
+            time.sleep(0.5)
+            self.update_grid()
 
 # Запуск игры
 if __name__ == "__main__":
-    game_of_life(width, height, 3)
+    game = GameOfLife(width=20, height=20)
+    game.run(iterations=2)
